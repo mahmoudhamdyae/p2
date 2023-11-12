e737a094e5334fedbffcbff619e981fdfe68b56d
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-12 16:16:45 +0200

    View Fridge

--- lib/model/amber.dart
+++ lib/model/amber.dart
@@ -1,20 +1,21 @@
+import 'fridge.dart';
+
 class Amber {
   int id;
   String name;
   String size;
   int user_id;
+  // Owner owner;
 
-  Amber(this.id, this.name, this.size, this.user_id);
+  Amber(this.id, this.name, this.size, this.user_id/*, this.owner*/);
 
   factory Amber.fromJson(Map<String, dynamic> json) {
     return Amber(
         json['id'] as int,
         json['name'] as String? ?? "",
-      // "",
         json['size'] as String? ?? "0",
-      // "",
-        json['user_id'] as int? ?? -1
-      // 3
+        json['user_id'] as int? ?? -1,
+      // Owner(json['owner']["name"]),
     );
   }
 }
