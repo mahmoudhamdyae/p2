d2860fe951a1f9ab26ffd920661a8f93e9f8a280
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-12 15:42:57 +0200

    View Fridge

--- lib/presentation/main/fridges/view_fridge/fridge_view.dart
+++ lib/presentation/main/fridges/view_fridge/fridge_view.dart
@@ -1,24 +1,50 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:testt/app/di.dart';
 import 'package:testt/model/fridge.dart';
 import 'package:testt/presentation/main/fridges/fridges_controller.dart';
 import 'package:testt/presentation/resources/strings_manager.dart';
 
+import '../../../common/state_renderer/state_renderer.dart';
+import '../../../component/empty.dart';
+
 class ViewFridgeView extends StatelessWidget {
+  FridgesController controller = instance<FridgesController>();
   final Fridge fridge;
 
-  const ViewFridgeView({super.key, required this.fridge});
+  ViewFridgeView({super.key, required this.fridge});
 
   @override
   Widget build(BuildContext context) {
-    FridgesController controller = instance<FridgesController>();
+    controller.showFridge(fridge.id);
     controller.showFridge(fridge.id);
 
     return Scaffold(
       appBar: AppBar(
         title: const Text(AppStrings.edit_fridge),
       ),
-      body: const Placeholder(),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return StateRenderer(
+              stateRendererType: StateRendererType.fullScreenLoadingState,
+              retryActionFunction: () {});
+        } else if (controller.error.value != '') {
+          return StateRenderer(
+              stateRendererType: StateRendererType.fullScreenErrorState,
+              message: controller.error.value.replaceFirst(
+                  "Exception: ", ""),
+              retryActionFunction: () async {
+                await controller.getFridges();
+              });
+        } else {
+          if (controller.fridges.value.isEmpty) {
+            return emptyScreen(context, "لا يوجد ثلاجات");
+          } else {
+            return Placeholder();
+          }
+        }
+      }),
     );
   }
 }
