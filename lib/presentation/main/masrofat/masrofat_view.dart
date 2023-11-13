83bff159c511f134706676817b9665a53e8c764b
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-13 11:15:26 +0200

    edit masrofat

--- lib/presentation/main/masrofat/masrofat_view.dart
+++ lib/presentation/main/masrofat/masrofat_view.dart
@@ -47,87 +47,7 @@ class MasrofatView extends StatelessWidget {
           if (controller.masrofat.value.isEmpty) {
             return emptyScreen(context, "لا يوجد مصروفات");
           } else {
-            return /*ListView.builder(
-              itemCount: controller.masrofat.value.length,
-              itemBuilder: (context, index) {
-                final item = controller.masrofat.value[index];
-
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ViewMasrofView(masrofat: item)));
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(AppPadding.p8),
-                    child: Card(
-                      elevation: AppSize.s8,
-                      child: Padding(
-                        padding: const EdgeInsets.all(AppPadding.p8),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  item.description,
-                                  style: TextStyle(
-                                    color: Theme.of(context).primaryColor,
-                                    fontSize: 23,
-                                  ),
-                                ),
-                                const SizedBox(
-                                  height: AppSize.s12,
-                                ),
-                                Text(
-                                  item.amount,
-                                  style: const TextStyle(
-                                    color: Colors.black38,
-                                    fontSize: 14,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                IconButton(
-                                  onPressed: () async {
-                                    showEditMasrofDialog(context, item);
-                                  },
-                                  icon: const Icon(
-                                    Icons.edit,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                                IconButton(
-                                  onPressed: () async {
-                                    return AwesomeDialog(
-                                        btnCancelText: "الغاء",
-                                        btnOkText: "حذف",
-                                        context: context,
-                                        dialogType: DialogType.noHeader,
-                                        title: "حذف",
-                                        desc: "هل أنت متأكد من حذف ${item.description} ؟",
-                                        btnCancelOnPress: () {},
-                                        btnOkOnPress: () async {
-                                          await controller.delMasrof(item.id);
-                                          await controller.getMasrofat();
-                                        }).show();
-                                  },
-                                  icon: const Icon(
-                                    Icons.delete,
-                                    color: Colors.black38,
-                                  ),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            )*/
+            return
               Column(
                 children: [
                   SizedBox(
@@ -266,46 +186,6 @@ class YourDataTableSource extends DataTableSource {
                     color: Colors.black38,
                   ),
                 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-                /*Row(
-                  children: [
-                    IconButton(
-                      onPressed: () async {
-                        showEditMasrofDialog(context, item);
-                      },
-                      icon: const Icon(
-                        Icons.edit,
-                        color: Colors.black38,
-                      ),
-                    ),
-
-                  ],
-                )*/
-
-
-
-
-
-
-
-
-
-
-
-
-
               ],
             )
         ),
