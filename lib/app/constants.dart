a52ef4b2c6f332b2bcac4b248c690797b0b65dea
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-15 08:42:38 +0200

    T

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -2,6 +2,6 @@ class Constants {
   static const String baseUrl = "https://0d93-196-153-64-250.ngrok-free.app/api/";
   static const String empty = "";
   static const String token = "SEND TOKEN HERE";
-  static const int zero = 0;
+  static const int zero = 0;;
   static const Duration apiTimeOut = Duration(minutes: 1);
 }
