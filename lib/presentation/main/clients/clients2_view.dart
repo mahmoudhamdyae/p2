d676b3258bea22e4e3bc6a27fd9d05d2f8f51d35
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-19 16:14:25 +0200

    Clients

--- lib/presentation/main/clients/clients2_view.dart
+++ lib/presentation/main/clients/clients2_view.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:testt/app/di.dart';
 import 'package:testt/presentation/main/clients/clients_controller.dart';
 
+import '../../resources/routes_manager.dart';
+import '../../resources/values_manager.dart';
+
 class Clients2View extends StatelessWidget {
   Clients2View({super.key});
   final ClientsController controller = instance<ClientsController>();
@@ -12,7 +15,171 @@ class Clients2View extends StatelessWidget {
       appBar: AppBar(
         title: const Text("عملاء"),
       ),
-      body: Placeholder(),
+      body: Padding(
+        padding: const EdgeInsets.all(AppPadding.p32),
+        child: Column(
+          children: [
+            Expanded(child: Container()),
+            // أفراد
+            Expanded(
+              child: GestureDetector(
+                onTap: () =>
+                {
+                  // Navigator.pushNamed(context, Routes.clients2Route)
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppSize.s16),
+                    color: Colors.white.withOpacity(.8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3)
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Row(
+                        children: [
+                          Expanded(child: Container()),
+                          Padding(
+                            padding: const EdgeInsets.only(right: AppPadding.p8),
+                            child: Text(
+                              "أفراد",
+                              style: TextStyle(
+                                color: Theme
+                                    .of(context)
+                                    .primaryColor,
+                                fontSize: 32,
+                              ),
+                            ),
+                          ),
+                          Expanded(child: Container()),
+                          Expanded(child: Container()),
+                          Padding(
+                            padding: const EdgeInsets.only(left: AppPadding.p8),
+                            child: Icon(Icons.arrow_forward_ios,
+                                size: 38,
+                                color: Theme
+                                    .of(context)
+                                    .primaryColor),
+                          ),
+
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: AppSize.s32,
+            ),
+            // التجار
+            Expanded(
+              child: GestureDetector(
+                onTap: () =>
+                {
+                  Navigator.pushNamed(context, Routes.periodsRoute)
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppSize.s16),
+                    color: Colors.white.withOpacity(.8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3)
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.schedule_sharp,
+                              size: 65,
+                              color: Theme
+                                  .of(context)
+                                  .primaryColor),
+                          const SizedBox(
+                            width: AppSize.s32,
+                          ),
+                          Text(
+                            "التجار",
+                            style: TextStyle(
+                              color: Theme
+                                  .of(context)
+                                  .primaryColor,
+                              fontSize: 25,
+                            ),
+                          )
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: AppSize.s32,
+            ),
+            // كل العملاء
+            Expanded(
+              child: GestureDetector(
+                onTap: () =>
+                {
+                  // Navigator.pushNamed(context, Routes.periodsRoute)
+                },
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(AppSize.s16),
+                    color: Colors.white.withOpacity(.8),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: const Offset(0, 3)
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: double.infinity,
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.schedule_sharp,
+                              size: 65,
+                              color: Theme
+                                  .of(context)
+                                  .primaryColor),
+                          const SizedBox(
+                            width: AppSize.s32,
+                          ),
+                          Text(
+                            "كل العملاء",
+                            style: TextStyle(
+                              color: Theme
+                                  .of(context)
+                                  .primaryColor,
+                              fontSize: 25,
+                            ),
+                          )
+                        ]),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(child: Container()),
+          ],
+        ),
+      ),
     );
   }
 }
