a0f0d454da38408737b60d65a9fd931bc8257329
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-22 13:31:47 +0200

    Clients

--- lib/model/client.dart
+++ lib/model/client.dart
@@ -10,12 +10,15 @@ class Client {
   String address;
   String vegetableName;
   String fridgeName;
-  // Amber amber;
-  // Fridge fridge;
-  // Term term
+  String amberName;
+  String termName;
+  int fridgeId;
+  int amberId;
+  int termId;
+  int priceId;
 
   Client(
-      this.id, this.name, this.phone, this.address, this.vegetableName, this.fridgeName//, this.amber, this.fridge, //this.term
+      this.id, this.name, this.phone, this.address, this.vegetableName, this.fridgeName, this.amberName, this.termName, this.fridgeId, this.amberId, this.termId, this.priceId
       );
 
   factory Client.fromJson(Map<String, dynamic> json) {
@@ -25,10 +28,13 @@ class Client {
       json['phone'] as String? ?? "",
       json['address'] as String? ?? "",
       json['vegetable_name'] as String? ?? "",
-      json['fridge_name'] as String? ?? "",
-      // Amber.fromJson(json["amber_details"]),
-      // Fridge.fromJson(json["amber_details"]["fridge"]),
-      // Term.fromJson(json[])
+      json['fridge'] as String? ?? "",
+      json['amber'] as String? ?? "",
+      json['term'] as String? ?? "",
+      json['fridge_id'] as int? ?? 0,
+      json['amber_id'] as int? ?? 0,
+      json['term_id'] as int? ?? 0,
+      json['price_list_id'] as int? ?? 0,
     );
   }
 }
