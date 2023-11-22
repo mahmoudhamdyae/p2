e83972f9020c228c655c29c8a8711a6a4e7357d9
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-22 19:53:47 +0200

    Clients

--- lib/presentation/main/clients/add_client.dart
+++ lib/presentation/main/clients/add_client.dart
@@ -24,6 +24,7 @@ class AddClient extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    controller.clearAll();
     return Scaffold(
       appBar: AppBar(
         title: const Text("إضافة عميل"),
