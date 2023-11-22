9a0b715813e55610293fae1b0bc66f4c22d767d2
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-22 18:01:48 +0200

    Clients

--- lib/presentation/main/clients/dialogs/resubscribe_dialog.dart
+++ lib/presentation/main/clients/dialogs/resubscribe_dialog.dart
@@ -7,6 +7,7 @@ import '../../../component/alert.dart';
 import '../../../component/empty.dart';
 import '../../../component/error.dart';
 import '../../../resources/strings_manager.dart';
+import '../../../resources/values_manager.dart';
 import '../clients_controller.dart';
 
 Future showResubscribeDialog(BuildContext context, String clientId, int fridgeId, int priceId, int termId) async {
@@ -20,7 +21,7 @@ Future showResubscribeDialog(BuildContext context, String clientId, int fridgeId
           showDialog(
             context: context,
             builder: (BuildContext context) {
-              return CustomDialog(fridgeId, clientId);
+              return CustomDialogAddTerm(clientId);
             },
           );
         });
@@ -32,7 +33,7 @@ Future showResubscribeDialog(BuildContext context, String clientId, int fridgeId
   }
 }
 
-class CustomDialog extends StatelessWidget {
+/*class CustomDialog extends StatelessWidget {
   final int fridgeId;
   final String clientId;
   CustomDialog(this.fridgeId, this.clientId, {super.key});
@@ -102,6 +103,125 @@ class CustomDialog extends StatelessWidget {
           ),
         )
         ,
+      ),
+    );
+  }
+}*/
+
+class CustomDialogAddTerm extends StatelessWidget {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController startController = TextEditingController();
+  TextEditingController endController = TextEditingController();
+  GlobalKey<FormState> formState = GlobalKey<FormState>();
+  final ClientsController controller = instance<ClientsController>();
+  String clientId;
+
+  CustomDialogAddTerm(this.clientId, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              "إضافة فترة",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            Form(
+              key: formState,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(AppPadding.p8),
+                    child: TextFormField(
+                      controller: nameController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.text,
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "يحب إضافة اسم للفترة";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "اسم الفترة",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(AppPadding.p8),
+                    child: TextFormField(
+                      controller: startController,
+                      textInputAction: TextInputAction.next,
+                      keyboardType: TextInputType.text,
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "يحب إضافة بداية للفترة";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "بداية الفترة",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1))),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(AppPadding.p8),
+                    child: TextFormField(
+                      controller: endController,
+                      textInputAction: TextInputAction.done,
+                      keyboardType: TextInputType.text,
+                      validator: (val) {
+                        if (val == null || val.isEmpty) {
+                          return "يحب إضافة نهاية للفترة";
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(
+                          hintText: "نهاية الفترة",
+                          border: OutlineInputBorder(
+                              borderSide: BorderSide(width: 1))),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () async {
+                var formData = formState.currentState;
+                if (formData!.validate()) {
+                  formData.save();
+                  try {
+                    showLoading(context);
+                    // await controller.addTerm(nameController.text, startController.text, endController.text)
+                    //     .then((userCredential) {
+                    //   Navigator.of(context).pop();
+                    //   Navigator.of(context).pop();
+                    // });
+                    showChoosePriceDialogDialog(context, clientId);
+                  } on Exception catch (e) {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pop();
+                    showError(context, e.toString());
+                  }
+                }
+              },
+              child: const Text("إضافة الفترة"),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -623,7 +743,7 @@ class CustomDialog6 extends StatelessWidget {
                     showLoading(context);
                     await controller.addAnbar(fridgeId, nameController.text)
                         .then((userCredential) {
-                          controller.getFridges();
+                          // controller.resubscribe(clientId);
                       Navigator.of(context).pop();
                       Navigator.of(context).pop();
                     });
