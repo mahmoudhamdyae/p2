d19842401101449666f97879c39a4f4d59a72863
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-28 11:17:19 +0200

    Initial Commit

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,5 +1,10 @@
 class Constants {
   static const String baseUrl = "https://e1e1-196-153-63-242.ngrok-free.app/api/";
+
+
+
+
+
   static const String empty = "";
   static const String token = "SEND TOKEN HERE";
   static const int zero = 0;
